61e7076e8f88f3b3958f3ab5da01afcf17db605d
graugaard <graugaard@gmail.com>
2018-05-23 09:02:33 +0200

    new project

---
+++ Variabler/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Variabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //heltal
+            Int32 a = 10;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            a--;
+            Console.WriteLine(a);
+            a +=20;
+            Console.WriteLine(a);
+            
+            //kommatal
+            double b;
+            b = 12.5;
+            Console.WriteLine(b);
+            b++;
+            Console.WriteLine(b);
+            b--;
+            Console.WriteLine(b);
+            b *= 2;
+            Console.WriteLine(b);
+
+            //Enum
+            FilTyper ft=0;
+            Console.WriteLine(ft);
+            Console.WriteLine((int)ft);
+
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today);
+            Console.WriteLine(today.ToString("ddMMyy"));
+            Console.WriteLine(today.ToUniversalTime());
+
+            Person p = new Person();
+            p.Id = 1;
+            p.Navn = "Mikkel";
+            Console.WriteLine(p.Id);
+            Console.WriteLine(p.Navn);
+
+            System.Console.ReadLine();
+        }
+    }
+
+    enum FilTyper
+    {
+        csv,pdf,txt
+    }
+
+    public struct Person
+    {
+        public int Id;
+        public string Navn;
+
+    }
+}
--- testConsoleApp/Program.cs
+++ testConsoleApp/Program.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace testConsoleApp
+namespace TestConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
 
+            System.Int32 i =1;
+            int ii;
+            ii = 11;
+            Console.WriteLine(int.MaxValue);
 
+            System.Byte b;
+            byte bb;
+
+            System.Double d = 0.11;
+
+            System.DateTime da;
+            da = DateTime.Now;
+            Console.WriteLine(da.ToString());
+
+            System.Console.ReadLine();
         }
     }
 }
